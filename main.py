4ae55f0b77c4dbe0b3e0a3caff4904d0f78fd990
Lion1446 <lionellediaz2557@gmail.com>
2023-10-19 10:36:58 +0800

    Added audit blueprint

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from endpoints.ingredient import ingredient_blueprint
 from endpoints.inventory import inventory_blueprint
 from endpoints.products import products_blueprint
 from endpoints.sales import sales_blueprint
+from endpoints.audit import audit_blueprint
 
 
 ## ======================= STARTUPS =================================
@@ -28,6 +29,7 @@ app.register_blueprint(ingredient_blueprint)
 app.register_blueprint(inventory_blueprint)
 app.register_blueprint(products_blueprint)
 app.register_blueprint(sales_blueprint)
+app.register_blueprint(audit_blueprint)
 
 
 ## ======================= ENDPOINTS =================================
