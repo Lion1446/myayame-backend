a2788b86609c53eb13e9d71c7fe6cf0dbd85351b
Lion1446 <lionellediaz2557@gmail.com>
2023-10-20 19:03:38 +0800

    added debug print errors

--- endpoints/user.py
+++ endpoints/user.py
@@ -70,6 +70,6 @@ def users():
         resp = make_response(response_body)
     except Exception as e:
         print(e)
-        resp = make_response({"status": 500, "remarks": "Internal server error"})
+        resp = make_response({"status": 500, "remarks": f"Internal server error: {e}"})
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
